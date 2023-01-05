efed373ffba79d83e496c8bc0913d81e7e78d8bc
YsDnG <ya.denguir@gmail.com>
2023-01-05 19:27:28 +0100

    Project Calculator v1.0

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 const buttons = document.querySelectorAll('input')
 const display = document.querySelector('.display')
+const lastResult = document.querySelector('.lastResult')
+const clickMe = document.querySelector('.clickme')
 
 let tabValueEntry= []
 let tabTypeEntry=[]
 let number=""
+let valeurs = []
+let operator= []
 let displayString=''
+display.innerText='0'
 
 
 function getValue(e)
 {
-  
-   
+    if(this.value ==='<=')
+       {
+         displayString =displayString.substring(0,displayString.length-1)
+         number=number.substring(0,number.length-1)
+         
+       }
     if(display.innerText == '0' && tabTypeEntry.includes('number'))
-        display.innerHTML=''
+        display.innerText=''
 
-        if(tabTypeEntry.length == 0 && this.dataset.type !== 'operator'||'number'=== this.dataset.type|| number =='' && this.dataset.type !== 'operator')
+        if((tabTypeEntry.length == 0 && this.dataset.type !== 'operator'||'number'=== this.dataset.type|| number =='' && this.dataset.type !== 'operator')&&this.dataset.type !=="delete")
             { 
                
                 number = number.concat('',this.value)
@@ -24,46 +33,26 @@ function getValue(e)
             }
         else
         {
-            if(!tabTypeEntry.length == 0 && tabTypeEntry.includes('number'))
+            if(!tabTypeEntry.length == 0 && tabTypeEntry.includes('number')&& this.dataset.type !=="delete")
             {
                 tabValueEntry.push(number)
                 tabValueEntry.push(this.value)
                 
                 displayString = displayString.concat('',this.value)
-                display.innerHTML = displayString
+                display.innerText = displayString
                 number= ""
             }
         }
 
-        display.innerHTML = displayString
+        display.innerText = displayString
 
-        
-        
-
-   
-    if(tabTypeEntry.length == 0 || tabTypeEntry[tabTypeEntry.length-1] !== this.dataset.type)
+    if((tabTypeEntry.length == 0 || tabTypeEntry[tabTypeEntry.length-1] !== this.dataset.type ) && this.dataset.type !=="delete")
             tabTypeEntry.push(this.dataset.type)
 
-        
-            
-    
- 
-  
-    
-   // console.log(tabValueEntry)
-            
-
-
- 
-        
-    
 
     if(this.value == '=')
     {
-        let valeurs = []
-        let operator= []
-        let summstotal=0
-
+       
         for(i=0;i<tabValueEntry.length-1;i++)
         {
             if(i%2 === 0)
@@ -76,132 +65,49 @@ function getValue(e)
              {
                 operator[i] = tabValueEntry[i]
                 operator = operator.filter(o=> o)
-                //console.log(operator)
+                
                
              }  
  
         }
-        if(operator.indexOf('x') == -1 && operator.indexOf('÷') == -1)
+      
+        while(valeurs.length && operator.length)
         {
-            total=valeurs[0]*1
-            valeurs.splice(0,1)
+
+        if(operator.indexOf('x')!== -1 && operator.indexOf('÷') !== -1)
+        {
+            ChoosePrio()
         }
-        else
+        if(operator.indexOf('x') !== -1)
+            multiply()
         
-            if(operator.indexOf('x') !== -1)
-            {
-                total= valeurs[operator.indexOf('x')]
-                valeurs.splice(operator.indexOf('x'),1)
-            }
-            
-            if(operator.indexOf('÷') !== -1)
-            {
-                valeurs[operator.indexOf('÷')]
-                valeurs.splice(operator.indexOf('÷'),1)
-            }
-            
-        z=0
-
-        while(valeurs.length)
-        {
-           if(operator.indexOf('x') !== -1)
-           {
-            if(operator.length>1 && !operator.some((value,index)=> operator.indexOf(value)===index))
-            {
-                total = total* valeurs[operator.indexOf('x')]*valeurs[operator.indexOf('x')+1]
-                valeurs.splice(operator.indexOf('x'),2)
-                operator.splice(operator.indexOf('x'),2)
-            }
-            else
-            {
-                total = total * valeurs[operator.indexOf('x')]
-                valeurs.splice(operator.indexOf('x'),1)
-                operator.splice(operator.indexOf('x'),1)
-            }
-                  
-           }
-           if(operator.indexOf('÷') !== -1)
-           {
-                
-                if(operator.length>1 && !operator.some((value,index)=> operator.indexOf(value)===index))
-                {
-                    total = total/valeurs[operator.indexOf('÷')]/ valeurs[operator.indexOf('÷')+1]
-                    valeurs.splice(operator.indexOf('÷'),2)
-                    operator.splice(operator.indexOf('÷'),2)
-                    
-                }
-                else
-                {
-                    total = total / valeurs[operator.indexOf('÷')]
-                    valeurs.splice(operator.indexOf('÷'),2)
-                    operator.splice(operator.indexOf('÷'),1)
-                }
-
-           }
            
-            if(operator[0] ==='+' && operator.indexOf('x') == -1 && operator.indexOf('÷') == -1)
-            {
-                if(valeurs.length ==1)
-                {    
-                total = valeurs[0]*1 +total *1
-                valeurs.splice(0,1)
-                operator.splice(0,1)
-                }
-                else{
-                    let summs=0
-                    summs = valeurs[0]*1+ valeurs[1]*1 
-                    valeurs.splice(0,2)
-                    operator.splice(0,1)
-                    valeurs[0] = summs;
-                }
+        if(operator.indexOf('÷') !== -1)
+            divide()
+        
+           
+        if(operator[0] ==='+')
+            summs()
                  
-            }
-            if(operator[0]==='-' && operator.indexOf('x') == -1 && operator.indexOf('÷') == -1)
-            {
-                if(valeurs.length ==1)
-                {   
-                total = valeurs[0]*1 - total *1
-                valeurs.splice(0,1)
-                operator.splice(0,1)
-                }
-                else{
-                    let summs=0
-                    summs= valeurs[0]*1 - valeurs[1]*1 
-                    valeurs.splice(0,2)
-                    operator.splice(0,1)
-                    valeurs[0] = summs;
-
-                }
-
-               
-            }
-           
             
+        if(operator[0]==='-')
+            soustract()
+ 
         }
         
         
         if(isNaN(total))
-        display.innerHTML= displayString+' Result is not a number'
+        display.innerText= displayString+' Result is not a number'
         else
-        display.innerHTML = displayString + total
+        display.innerText = displayString + total
+        lastResult.innerText = "="+total
         total=0
+        reset()
 
-
-
-
-        // if(tabValueEntry[1]==='+')
-        //     display.innerHTML=`${tabValueEntry[0]} ${tabValueEntry[1]} ${tabValueEntry[2]} = ${tabValueEntry[0]*1 + tabValueEntry[2]*1}`
-        // if(tabValueEntry[1] === '-')
-        //     display.innerHTML=` ${tabValueEntry[0]} ${tabValueEntry[1]} ${tabValueEntry[2]} = ${tabValueEntry[0]*1 - tabValueEntry[2]*1}`
-        // if(tabValueEntry[1] === 'x')
-        //     display.innerHTML=`${tabValueEntry[0]} ${tabValueEntry[1]} ${tabValueEntry[2]} = ${tabValueEntry[0]*1 * tabValueEntry[2]*1}`
-        //     if(tabValueEntry[1] === '÷')
-        //     display.innerHTML=`${tabValueEntry[0]} ${tabValueEntry[1]} ${tabValueEntry[2]} = ${tabValueEntry[0]*1 / tabValueEntry[2]*1}`
-        //     reset()
     }
    
 
-    if(this.dataset.type==='delete')
+    if(this.value==='Del')
     {
         reset()
         clearDisplay()
@@ -218,13 +124,115 @@ function reset()
     tabValueEntry=[]
     displayString=''
 }
+function multiply()
+{
+            total = valeurs[operator.indexOf('x')+1] * valeurs[operator.indexOf('x')]
+            valeurs.splice(operator.indexOf('x'),2)
+            valeurs.splice(operator.indexOf('x'),0,total)
+            operator.splice(operator.indexOf('x'),1)
+            if(valeurs.length == 1)
+            {
+                total= valeurs[0]
+                valeurs.splice(0,1)
+            }
+
+}
+
+function divide()
+{
+    total = valeurs[operator.indexOf('÷')] / valeurs[operator.indexOf('÷')+1]
+                valeurs.splice(operator.indexOf('÷'),2)
+                
+                valeurs.splice(operator.indexOf('÷'),0,total)
+                operator.splice(operator.indexOf('÷'),1)
+                if(valeurs.length == 1)
+                {
+                    total= valeurs[0]
+                    valeurs.splice(0,1)
+                }
+
+}
+function summs()
+{
+    total = valeurs[operator.indexOf('+')]*1+valeurs[operator.indexOf('+')+1]*1
+    valeurs.splice(operator.indexOf('+'),2)
+    valeurs.splice(operator.indexOf('+'),0,total)
+    operator.splice(operator.indexOf('+'),1)
+    if(valeurs.length == 1)
+    {
+        total= valeurs[0]
+        valeurs.splice(0,1)
+    }
+}
+function soustract()
+{
+    total = valeurs[operator.indexOf('-')]-valeurs[operator.indexOf('-')+1]
+                valeurs.splice(operator.indexOf('-'),2)
+                valeurs.splice(operator.indexOf('-'),0,total)
+                operator.splice(operator.indexOf('-'),1)
+                if(valeurs.length == 1)
+                {
+                    total= valeurs[0]
+                    valeurs.splice(0,1)
+                }
+
+}
+
 function clearDisplay()
 {
-    display.innerHTML='0'
+    display.innerText='0'
+}
+
+function ChoosePrio()
+{
+    if(operator.indexOf('x')< operator.indexOf('÷'))
+        multiply()
+    else
+        divide()
+    
 }
 
 buttons.forEach(button => { 
-    if(!button.classList.contains('red'))
+    if(!button.classList.contains('clickme'))
         button.addEventListener('click',getValue)
+
+})
+
+document.addEventListener('keydown',(e)=>{
+    buttons.forEach(button => {
+            let valeur = parseInt(button.dataset.key)
+
+            if(valeur === e.keyCode)
+            {
+                button.click()
+            }
+            console.log(e)
+ 
+            
+    })
+    
+})
+
+clickMe.addEventListener('click',(e)=>{
+    e.preventDefault()
+    if(document.querySelector('.container').style.display==="none")
+    {
+        document.querySelector('.container').style.display='flex'
+        document.body.style.backgroundImage ='none'
+        clickMe.value = "You should listen, now you are stuck with me !"
+        clickMe.style.backgroundColor ="red"
+        
+        clickMe.classList.add('clicked')
+       
+        clickMe.disabled=true
+    }
+    else
+    {
+    document.querySelector('.container').style.display='none'
+    document.body.style.backgroundImage ='url(../img/that-was-boring.jpg)'
+    clickMe.value = 'Why?'
+    clickMe.style.backgroundColor = 'white'
+    clickMe.style.color = "black"
+    }
 
 })
